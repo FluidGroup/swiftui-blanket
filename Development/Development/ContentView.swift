cf8ce829e24a06fab7cbc79502688c5d5f1b1354
Muukii <muukii.app@gmail.com>
2024-07-27 01:15:50 +0900

    Update

---
+++ Development/Development/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Development
+//
+//  Created by Muukii on 2024/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Development/DevelopmentTests/DevelopmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopmentTests.swift
+//  DevelopmentTests
+//
+//  Created by Muukii on 2024/07/27.
+//
+
+import Testing
+@testable import Development
+
+struct DevelopmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
